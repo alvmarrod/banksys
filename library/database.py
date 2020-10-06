396244473f7922b6ca342e7a28e901a2fc84525c
Alvaro MR <btc@hotmail.es>
2020-10-07 01:30:24 +0200

    Added basic database interface for sqlite3 and testing directly in main script

--- library/database.py
+++ library/database.py
@@ -4,5 +4,115 @@ import sqlite3
 
 #######################################################################
 
+def _split_query_result(row, sep=",", quot="\""):
+    """Splits the given string by separator symbol, taking into account
+    the quote symbol.
+    """
+
+    # TODO
+    pass
 
 #######################################################################
+
+def _create_all_movements_table(con):
+    """Creates the table for consolidated data. This function is
+    idempotent, so it will not overwrite an existing config file.
+    """
+
+    tb_exists = "SELECT name FROM sqlite_master WHERE type='table' " + \
+                "AND name='all_movements'"
+
+    if not con.execute(tb_exists).fetchone():
+
+        logging.info("Table all_movements not detected!")
+
+        con.execute('''CREATE TABLE all_movements
+            (ID INTEGER PRIMARY KEY,
+            OP_DATE        TEXT    NOT NULL,
+            VAL_DATE       TEXT    NOT NULL,
+            CONCEPT        TEXT    NULL,
+            AMOUNT         REAL,
+            BALANCE        REAL);''')
+
+        logging.info("Table all_movements created!")
+
+#######################################################################
+
+def _execute_non_reader_query(con, query) -> int:
+    """Execute a non-reader query that returns a `int` value indicating
+    if the execution was successful.
+
+    If the command executed was an `update`, then the returned value
+    indicates the number of rows that have been updated.
+    """
+
+    result = 0
+    cursor = con.execute(query)
+
+    commit_commands = [
+        "insert",
+        "update",
+        "delete"
+    ]
+
+    if query.split(" ")[0].lower() in commit_commands:
+        con.commit()
+
+    if query.split(" ")[0].lower() == "update":
+        result = con.total_changes
+
+    return result
+
+def _execute_reader_query(con, query) -> list:
+    """Execute a reader query that returns a `list` with the query
+    response.
+    """
+
+    cursor = con.execute(query)
+    rows = cursor.fetchall()
+
+    return rows
+
+
+#######################################################################
+
+def open_database(db_file) -> sqlite3.Connection:
+    """Open the specified file as a sqlite3 database file.
+
+    Parameters:
+    - A `str` with the filename of the database without extension
+
+    Returns:
+    - A `sqlite3.Connection` with the connection for the database
+    """
+
+    filepath = f"./data/{db_file}.db"
+    con = None
+
+    try:
+        con = sqlite3.connect(filepath)
+        _create_all_movements_table(con)
+
+    except Exception as e:
+
+        logging.warning("Couldn't connect to the specified database" + \
+                        f" \"{db_file}\"")
+        logging.info(e)
+
+    return con
+
+def close_database(con):
+    """Close the given database connection
+
+    Parameters:
+    - A `sqlite3.Connection` to be closed
+    """
+
+    try:
+        con.close()
+
+    except Exception as e:
+
+        logging.warning("Couldn't close the specified database" + \
+                        " \"{db_file}\"")
+        logging.info(e)
--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import pandas as pd
 import library.UI as UI
 import library.analysis as analysis
 import library.yamlconfig as YC
+import library.database as DB
 
 #######################################################################
 
@@ -14,24 +15,21 @@ logging.basicConfig(level=logging.INFO,
                     format='%(name)s - %(levelname)s - %(message)s')
 
 SETUP_PROFILE = "User1"
+DB_FILE = "consolidated_data"
 
 #######################################################################
 
 if __name__ == "__main__":
 
-    parser = argparse.ArgumentParser(description='Process a CSV file with ' \
-        + 'bank account movements')
-
-    parser.add_argument('--file', default=None,
-                        help='CSV File to process')
-
-    args = parser.parse_args()
-
-    if not args.file:
-        parser.print_help()
-        exit(1)
-
-    logging.info(f"File path: {args.file}")
+    # parser = argparse.ArgumentParser(description='Process a CSV file with ' \
+    #     + 'bank account movements')
+    # parser.add_argument('--file', default=None,
+    #                     help='CSV File to process')
+    # args = parser.parse_args()
+    # if not args.file:
+    #     parser.print_help()
+    #     exit(1)
+    # logging.info(f"File path: {args.file}")
 
     # print(f"1. Loading file...")
     # movements = processes.load_movements(args.file)
@@ -47,4 +45,20 @@ if __name__ == "__main__":
 
     configuration = YC.load_config(SETUP_PROFILE)
 
-    UI.menu()
+    con = DB.open_database(DB_FILE)
+
+    res = DB._execute_reader_query(con, "SELECT ID, OP_DATE, VAL_DATE, CONCEPT, AMOUNT, BALANCE from all_movements")
+    print(f"Reads result: {len(res)}")
+    for item in res:
+        print(item)
+    res = DB._execute_non_reader_query(con, "INSERT INTO all_movements(OP_DATE, VAL_DATE, CONCEPT, AMOUNT, BALANCE) VALUES (\"2020-20-06\",\"2020-20-06\", \"Concepto prueba jeje\", 50, 50)")
+    print("Insert result: ")
+    print(res)
+    res = DB._execute_reader_query(con, "SELECT ID, OP_DATE, VAL_DATE, CONCEPT, AMOUNT, BALANCE from all_movements")
+    print(f"Reads result: {len(res)}")
+    for item in res:
+        print(item)
+
+    DB.close_database(con)
+
+    # UI.menu()
